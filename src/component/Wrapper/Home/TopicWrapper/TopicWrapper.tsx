62d86eff9732647e33c6cec1efac19f57cdb0d43
Shota Takahasi <shota.takahasisan@outlook.com>
2024-06-16 03:30:45 +0900

    added topicwrapper component

--- src/component/Wrapper/Home/TopicWrapper/TopicWrapper.tsx
+++ src/component/Wrapper/Home/TopicWrapper/TopicWrapper.tsx
@@ -1,7 +1,48 @@
+"use client"
+
+import clsx from "clsx";
 import styles from "./TopicWrapper.module.css";
+import { useState } from "react";
 
 export const TopicWrapper = () => {
+  const [active, setActive] = useState("topics");
+
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div
+        className={clsx(styles.item, active == "topics" ? styles.active : styles.normal)}
+        onClick={() => setActive("topics")}
+      >
+        Topics
+      </div>
+
+      <div
+        className={clsx(styles.item, active == "dispute" ? styles.active : styles.normal)}
+        onClick={() => setActive("dispute")}
+      >
+        Azerbaijan-Armenia dispute
+      </div>
+
+      <div
+        className={clsx(styles.item, active == "covid" ? styles.active : styles.normal)}
+        onClick={() => setActive("covid")}
+      >
+        covid-19
+      </div>
+
+      <div
+        className={clsx(styles.item, active == "eastern" ? styles.active : styles.normal)}
+        onClick={() => setActive("eastern")}
+      >
+        eastern mediterranean
+      </div>
+
+      <div
+        className={clsx(styles.item, active == "turkey" ? styles.active : styles.normal)}
+        onClick={() => setActive("turkey")}
+      >
+        my turkey
+      </div>
+    </div>
   );
 };
