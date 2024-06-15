892a0e4698905784288521ce6af7c01d0892c259
Shota Takahasi <shota.takahasisan@outlook.com>
2024-06-16 05:39:42 +0900

    added TopicNewsCard component

--- src/component/Card/TopicNewsCard/TopicNewsCard.tsx
+++ src/component/Card/TopicNewsCard/TopicNewsCard.tsx
@@ -2,6 +2,14 @@ import styles from "./TopicNewsCard.module.css";
 
 export const TopicNewsCard = () => {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        Africa
+      </div>
+
+      <div className={styles.description}>
+        Hundreds of prisoners escape Uganda prison
+      </div>
+    </div>
   );
 };
