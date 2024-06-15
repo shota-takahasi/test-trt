40a6fff626adfd3a8542202650f39d915bb2a9c2
Shota Takahasi <shota.takahasisan@outlook.com>
2024-06-16 02:15:00 +0900

    finished header component

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,7 +1,49 @@
+import Link from "next/link";
 import styles from "./Header.module.css";
 
 export const Header = () => {
   return (
-    <div></div>
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <img
+          className={styles.logo}
+          src="./svg/header-logo.svg"          
+          alt="logo"
+        />
+
+        <div className={styles.content}>
+          <div className={styles.linkContent}>
+            <Link href="/" className={styles.link}>
+              News
+            </Link>
+
+            <Link href="/" className={styles.link}>
+              Features
+            </Link>
+
+            <Link href="/" className={styles.link}>
+              Topics
+            </Link>
+
+            <Link href="/" className={styles.link}>
+              Video
+            </Link>
+          </div>
+
+          <Link href="/" className={styles.live}>
+            <span className={styles.status}></span>
+            <div className={styles.link}>Live</div>
+          </Link>
+
+          <div className={styles.search}>
+            <img
+              className={styles.searchIcon}
+              src="./svg/search.svg"
+              alt="search"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
