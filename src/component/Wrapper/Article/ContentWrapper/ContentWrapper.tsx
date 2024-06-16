d026f27e310b33756ab1fc4ef0f7406563e7220a
Shota Takahasi <shota.takahasisan@outlook.com>
2024-06-17 08:16:44 +0900

    updated Article page ui responsive

--- src/component/Wrapper/Article/ContentWrapper/ContentWrapper.tsx
+++ src/component/Wrapper/Article/ContentWrapper/ContentWrapper.tsx
@@ -2,6 +2,32 @@ import styles from "./ContentWrapper.module.css";
 
 export const ContentWrapper = () => {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.topic}>
+          <img
+            className={styles.topicImage}
+            src="https://cdn-i.pr.trt.com.tr/trtworld/16358427_0-57-8256-4649.jpeg"
+            alt="image"
+          />
+
+          <div className={styles.topicTitle}>
+            Reuters
+          </div>
+
+          <div className={styles.topicDescription}>
+            Orange County Registrar in Santa Ana, California, US, October, 5, 2020.
+          </div>
+        </div>
+
+        <div className={styles.description}>
+          Americans are rushing to cast ballots ahead of the November election at an unprecedented pace, early voting numbers show, indicating a possible record turnout for the showdown between President Donald Trump and Democratic rival Joe Biden.
+        </div>
+
+        <div className={styles.tag}></div>
+      </div>
+
+      <div className={styles.related}></div>
+    </div>
   );
 };
